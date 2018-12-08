20ea614cd8f2f5e0fe4985c746ca5a7b01a93d01
maciek <maciek@sparkmedia.pl>
2018-12-09 00:27:33 +0100

    init

---
+++ src/main/java/pl/sparkidea/demo/ws/springwsdemo/SpringWsDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.sparkidea.demo.ws.springwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWsDemoApplication.class, args);
+    }
+}
